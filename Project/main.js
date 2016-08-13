56ffba3a2d23dabe7855957bd4a090aad46c8021
Nicola Fiori (JD342) <nicola@jd342.net>
2016-08-13 23:31:25 +0200

    Make window borderless & transparent

--- Project/main.js
+++ Project/main.js
@@ -1,12 +1,14 @@
 const { app, BrowserWindow } = require('electron');
 
-let win;
-
 app.on('ready', () => {
 
-    win = new BrowserWindow({ width: 800, height: 600 });
+    var win = new BrowserWindow({
+        title: "Sketcher",
+        frame: false,
+        transparent: true
+    });
+    win.setMenu(null);
     win.loadURL(`file://${__dirname}/main.htm`);
-    win.on('closed', () => { win = null; });
 
 });
 
