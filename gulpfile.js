8c358ca36cd45fe7ed632831abeec4552a8c1784
Nicola Fiori (JD342) <nicola@jd342.net>
2017-02-14 21:38:41 +0100

    Send Alt+Tab keystroke when window loads
    Sends the keystroke to regain focus of last active window. This is done
    in order to avoid stealing the focus when writing in an editor and a
    re-instantiation of the application occurs.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const { createInterface } = require('readline');
 const { exec } = require('mz/child_process');
 const { spawn } = require('child_process');
 const ab = require('auto-bind-proxy');
 const { task, watch, parallel } = ab(require('gulp'));
 const listenDeath = require('death');
+const timeout = require('timeout-as-promise');
 
 task('default', ['run'], () => {
     watch('source/**/*', ['run']);
@@ -25,6 +27,35 @@ task('default', ['run'], () => {
         ], { detached: true });
 
         pid = electron.pid;
+
+        // Sends Alt+Tab keystroke when window is opened to regain focus
+        // of last active window
+        {
+
+            const interf = createInterface({ input: electron.stdout });
+
+            interf.on('line', function onLine(line) {
+
+                if (line === 'load') {
+
+                    interf.removeListener('line', onLine);
+
+                    console.log('sending alt+tab keystroke');
+
+                    (async () => {
+                        await exec('xdotool keydown alt');
+                        await timeout(500);
+                        await exec('xdotool key Tab keyup alt');
+                    })().catch(() => {
+                        console.log('keystroke failed to execute, ' +
+                                    'xdotool may not be installed');
+                    });
+
+                }
+
+            });
+
+        }
 
         const unlistenDeath = listenDeath((signal) => {
             process.kill(-pid);
