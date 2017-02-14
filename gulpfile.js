85d7d6538f49d03909eb48d3b4c6f1e533756b6e
Nicola Fiori (JD342) <nicola@jd342.net>
2017-02-14 21:53:21 +0100

    Describe all behavior

--- gulpfile.js
+++ gulpfile.js
@@ -18,18 +18,30 @@ task('default', ['run'], () => {
 
     task('run', () => {
 
+        // If exists, kills the previous instance of the application
+        //
         if (pid !== null) process.kill(-pid);
 
+        // Instantiates the application
+        //
+        //   It is intantiated in detached mode because the application spawns
+        //   its own processes, therefore simply invoking the electron.kill()
+        //   method to close the application wouldn't work, an invocation to
+        //   process.kill(-pid) is needed.
+        //
         const electron = spawn('node_modules/.bin/electron', [
             '--enable-transparent-visuals',
             '--js-flags="--harmony"',
             'source/main.js'
         ], { detached: true });
 
+        // Caches the process identifier of the application
+        //
         pid = electron.pid;
 
         // Sends Alt+Tab keystroke when window is opened to regain focus
         // of last active window
+        //
         {
 
             const interf = createInterface({ input: electron.stdout });
@@ -57,11 +69,15 @@ task('default', ['run'], () => {
 
         }
 
+        // Kills the application instance when main process concludes
+        //
         const unlistenDeath = listenDeath((signal) => {
             process.kill(-pid);
             process.exit(signal);
         });
 
+        // Unlinks listener when the application concludes
+        //
         electron.once('exit', () => { unlistenDeath(); });
 
     });
